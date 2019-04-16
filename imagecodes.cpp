dfa10fb0622f13e91f99b8a506ef3ce1110891bb
Thomas Hoffmann <thomas.hoffmann@tualo.de>
2019-04-16 21:15:22 +0200

    remove includes

--- imagecodes.cpp
+++ imagecodes.cpp
@@ -1,7 +1,7 @@
 
 
-#include <opencv2/objdetect.hpp>
-#include <opencv2/imgcodecs.hpp>
+//#include <opencv2/objdetect.hpp>
+//#include <opencv2/imgcodecs.hpp>
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <zbar.h>
